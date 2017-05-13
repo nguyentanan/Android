b08b48e02845cfa24fa5ac45afb47ad835299acc
nguyentanan <ntanan1996@gmail.com>
2017-05-13 18:35:03 +0700

    Bai 13 Thuc hanh ve ListView trong Android

---
+++ Vidu_ListView_Xml_Array/gen/com/example/vidu_listview_xml_array/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.vidu_listview_xml_array;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
